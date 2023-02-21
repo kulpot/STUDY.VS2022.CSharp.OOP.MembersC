3d4c80b2ed95d13ade87fe80f287c6c94fa800fe
Kulpot <kulpotwahu@gmail.com>
2023-02-21 22:13:46 +0800

    Add project files.

---
+++ MembersC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MembersC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Members member1 = new Members();
+            member1.Introducing(true);
+            Console.ReadKey();
+        }
+
+
+    }
+}
